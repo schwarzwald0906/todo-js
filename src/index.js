21ed084365ccce3b65ff30f97e3c054b32319dc4
schwarzwald0906 <93188453+schwarzwald0906@users.noreply.github.com>
2022-07-16 15:56:54 +0900

    未完了のTODOの追加（テキスト）

--- src/index.js
+++ src/index.js
@@ -4,7 +4,16 @@ const onClickAdd = () => {
   //テキストボックスの値を取得し、値を初期化する。
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
-  alert(inputText);
+
+  //未完了のTODOの追加
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  div.appendChild(li);
+  document.getElementById("incomplete-list").appendChild(div);
 };
 
 document
