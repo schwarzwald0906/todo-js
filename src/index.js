70cc5ba78e19e66461f882bce538786068032a50
schwarzwald0906 <93188453+schwarzwald0906@users.noreply.github.com>
2022-07-16 17:19:26 +0900

    完了ボタンの機能を実装

--- src/index.js
+++ src/index.js
@@ -16,10 +16,36 @@ const onClickAdd = () => {
   li.innerText = inputText;
 
   //buttonタグ作成
+  //完了ボタン
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
-  completeButton.addEventListener("click", () => console.log("完了"));
+  completeButton.addEventListener("click", () => {
+    //完了したTODOに追加
+    //divタグ作成
+    const div = document.createElement("div");
+    div.className = "list-row";
 
+    //liタグ作成
+    const li = document.createElement("li");
+    li.innerText = inputText;
+
+    //戻すbuttonタグ追加
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+    backButton.addEventListener("click", () => alert("戻す"));
+
+    //画面表示
+    div.appendChild(li);
+    div.appendChild(backButton);
+    document.getElementById("complete-list").appendChild(div);
+
+    //未完了のTODOから削除
+    // const completeTarget = completeButton.parentNode;
+    const completeTarget = completeButton.parentNode;
+    document.getElementById("incomplete-list").removeChild(completeTarget);
+  });
+
+  //削除ボタン
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
   deleteButton.addEventListener("click", () => {
