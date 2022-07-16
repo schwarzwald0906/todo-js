7a68f6ba722f8aa608bbaac6e3247c57a364be13
schwarzwald0906 <93188453+schwarzwald0906@users.noreply.github.com>
2022-07-16 16:30:43 +0900

    未完了のTODOの追加(ボタン)

--- src/index.js
+++ src/index.js
@@ -6,13 +6,28 @@ const onClickAdd = () => {
   document.getElementById("add-text").value = "";
 
   //未完了のTODOの追加
+
+  //divタグ作成
   const div = document.createElement("div");
   div.className = "list-row";
 
+  //liタグ作成
   const li = document.createElement("li");
   li.innerText = inputText;
 
+  //buttonタグ作成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => console.log("完了"));
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => console.log("削除"));
+
+  //画面表示
   div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
   document.getElementById("incomplete-list").appendChild(div);
 };
 
